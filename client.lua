91c54160f609a1828a42d3937423599b7f6c3f22
Felix <ribeiro.felix@live.com>
2014-04-12 18:07:50 -0300

    describe agora com setup

--- client.lua
+++ client.lua
@@ -1,51 +1,57 @@
-local rpc = require "luarpc"
-
-function counsumeIpPort(interface)
+describe( "Lua client tests" , function ()
 	
-	local text = io.open("deploy", "r" ):read("*a")
+setup(function ()
+	rpc = require "luarpc"
 
-	local linestbl = {}
-	if text ~= "" then
-		iteLines = string.gmatch(text,'[^\r\n]+') 
-		for line in  iteLines do 
-			table.insert(linestbl,line)
-			
-			if(interface == line) then
-				-- if the current line is the required interface
-				-- we'll consume the next line to get ip/port
-				ipportline = iteLines()
-				if ipportline then
-					local ipportmatch = string.gmatch( ipportline , "%S+" )
-					ip, port = ipportmatch() , ipportmatch()	
+	function counsumeIpPort(interface)
+		
+		local text = io.open("deploy", "r" ):read("*a")
+
+		local linestbl = {}
+		if text ~= "" then
+			iteLines = string.gmatch(text,'[^\r\n]+') 
+			for line in  iteLines do 
+				table.insert(linestbl,line)
+				
+				if(interface == line) then
+					-- if the current line is the required interface
+					-- we'll consume the next line to get ip/port
+					ipportline = iteLines()
+					if ipportline then
+						local ipportmatch = string.gmatch( ipportline , "%S+" )
+						ip, port = ipportmatch() , ipportmatch()	
+					end
+					break
 				end
-				break
 			end
-		end
-		for line in iteLines do
-			table.insert(linestbl,line)
-		end
+			for line in iteLines do
+				table.insert(linestbl,line)
+			end
 
-		local file = io.open("deploy","w")
-		file:write(table.concat(linestbl,"\n") .. "\n")
-		file:close()
-		return ip,port
+			local file = io.open("deploy","w")
+			file:write(table.concat(linestbl,"\n") .. "\n")
+			file:close()
+			return ip,port
+		end
 	end
-end
 
-ip , port = counsumeIpPort("interface1.lua")
-p1 = rpc.createProxy( ip, port , "interface1.lua")
-print("Proxy 1 created in host " .. ip .. " and port " .. port)
+	ip , port = counsumeIpPort("interface1.lua")
+	p1 = rpc.createProxy( ip, port , "interface1.lua")
+	print("Proxy 1 created in host " .. ip .. " and port " .. port)
 
-ip , port = counsumeIpPort("interface1.lua")
-p2 = rpc.createProxy( ip, port , "interface1.lua")
-print("Proxy 2 created in host " .. ip .. " and port " .. port)
+	ip , port = counsumeIpPort("interface1.lua")
+	p2 = rpc.createProxy( ip, port , "interface1.lua")
+	print("Proxy 2 created in host " .. ip .. " and port " .. port)
 
-ip , port = counsumeIpPort("interface2.lua")
-p3 = rpc.createProxy( ip, port , "interface2.lua")
-print("Proxy 3 created in host " .. ip .. " and port " .. port)
+	ip , port = counsumeIpPort("interface2.lua")
+	p3 = rpc.createProxy( ip, port , "interface2.lua")
+	print("Proxy 3 created in host " .. ip .. " and port " .. port)
+
+end )
 
 
-describe("Proxy 1 tests", function()
+
+describe("Proxy #p1 tests", function()
 
   it("Ok expected tests" , function ()
   	local x , y = p1.foo(3, 5)
@@ -65,9 +71,7 @@ describe("Proxy 1 tests", function()
   end)
 end)
 
-
-
-describe("Proxy 2 tests", function()
+describe("Proxy #p2 tests", function()
   
   it("Ok expected tests" , function ()
   	local x , y = p2.foo(3, 5)
@@ -79,10 +83,7 @@ describe("Proxy 2 tests", function()
   end)
 end)
 
-
-
-
-describe("Proxy 3 tests", function()
+describe("Proxy #p3 tests", function()
   
   it("Ok expected tests" , function ()
   	local x , y = p3.foo(1, 2, 3)
@@ -98,3 +99,6 @@ end)
 
 
 
+
+
+end )
